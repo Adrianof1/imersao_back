e991884622e7e97f18041cc568a1ba8280f40ea0
Adrianof1 <adrianoferreira8@outlokk.com>
2024-11-20 18:29:12 -0300

    alterações

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
 import express from "express";
-import conectarAoBanco from "./src/config/dbconfig.js";
+import routes from "./src/routes/postsRoutes.js;
 
-// Conecta ao banco de dados usando a string de conexão fornecida pela variável de ambiente
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
-
-// Inicializa o aplicativo Express
 const app = express();
 
+routes(app);
 
-
-// Inicia o servidor na porta 3000
 app.listen(3000, () => {
-    console.log("Servidor escutando na porta 3000");
+    console.log("Servidor escutando...");
 });
-
-// Função assíncrona para obter todos os posts do banco de dados
-async function getTodosPosts() {
-    // Obtém o banco de dados 'imersao-instabytes'
-    const db = conexao.db("imersao-instabytes");
-    // Obtém a coleção 'posts'
-    const colecao = db.collection("posts");
-    // Retorna todos os documentos da coleção como um array
-    return colecao.find().toArray();
-}
-
-
-
 // function buscarPostPorID(id) {
 //   return posts.findIndex((post) => {
 //     return post.id === Number(id)
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,21 +1,10 @@
-import express from "express"
+import express from "express";
+import { listarPosts } from "../controllers/postsController.js";
 
 const routes = (app) => {
-    // Permite que o Express entenda requisições com corpo em formato JSON
     app.use(express.json());
-    // Rota para obter todos os posts
-    app.get("/posts", async (req, res) => {
-    try {
-        // Chama a função para obter os posts
-        const posts = await getTodosPosts();
-        // Envia os posts como resposta em formato JSON
-        res.status(200).json(posts);
-    } catch (error) {
-        // Caso ocorra um erro, registra no console e envia uma mensagem de erro
-        console.error("Erro ao obter os posts:", error);
-        res.status(500).json({ message: "Erro interno do servidor" });
-    }
-});
+    
+    app.get("/posts", listarPosts);
 }
 
 export default routes;
